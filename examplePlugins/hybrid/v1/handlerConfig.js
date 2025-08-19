8bff8a79bdd7c7057646856f4442c8b4de55c1cc
Shaswot Subedi <shaswot.subedi@squaredup.com>
2025-08-19 13:28:05 +0100

    example plugins update

--- examplePlugins/hybrid/v1/handlerConfig.js
+++ examplePlugins/hybrid/v1/handlerConfig.js
@@ -1,3 +1,4 @@
+import { HandlerFunctionEnum } from './handler.js';
 import { stageApps } from './importObjects/apps.js';
 import { stageBuildings } from './importObjects/building.js';
 import { appScopedProperties } from './readDataSource/appScopedProperties.js';
@@ -12,32 +13,36 @@ export async function testConfig(context) {
 
     if (typeof context.pluginConfig.serverUrl === 'string' && context.pluginConfig.serverUrl.startsWith('https:')) {
         messages.push({
-            status: 'success',
-            message: 'Testing passed'
+            'status': 'success',
+            'message': 'Testing passed'
         });
     } else {
         messages.push({
-            status: 'warning',
-            message: 'serverUrl is invalid'
+            'status': 'warning',
+            'message': 'serverUrl is invalid'
         });
         messages.push({
-            status: 'error',
-            message: 'nothing works!'
+            'status': 'error',
+            'message': 'nothing works!'
         });
     }
 
-    const result = {
+    const result =  {
         link: 'https://yourCompany.com/docs/plugin/pluginsetup-examplehybrid',
         messages: messages
     };
     return result;
+
 }
 
 // ============================================================================
 //
 // importObjects
 //
-export const importStages = [stageApps, stageBuildings];
+export const importStages = [
+    stageApps,
+    stageBuildings
+];
 
 export const defaultApiLimits = {
     apps: 10,
@@ -70,3 +75,37 @@ export const dataSourceFns = {
     appScopedProperties,
     dataSourceUnscoped
 };
+
+// ============================================================================
+//
+// oAuth
+//
+export async function authBegin(context) {
+    // Only needed if the plugin requires OAuth - consult documentation for more information
+    throw new Error('Not implemented');
+}
+
+export async function authCodeResponse(context) {
+    // Only needed if the plugin requires OAuth - consult documentation for more information
+    throw new Error('Not implemented');
+}
+
+
+// ============================================================================
+//
+// Generic
+//
+export async function configureContext(context, functionName) {
+    // Used to apply universal configuration to your context object
+    // For instance, a client from a library or a frequently used value
+    context.retryCount = 0;
+
+    // You can use the function name to take different actions depending on the function being called
+    if ([HandlerFunctionEnum.oAuth2, HandlerFunctionEnum.testConfig].includes(functionName)) {
+        // For example, when dealing with OAuth or configuration tests, you might prefer handling certain tasks manually
+        return;
+    }
+
+    // Track what warnings have been sent to the user so we don't bombard them with the same message
+    context.warnings = new Set();
+}
